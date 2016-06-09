d857eb03ef2b1af1ebb04196bbc57c882931a6de
Joao Gatto <joaocarlosgatto@hotmail.com>
2016-06-08 23:18:14 -0300

    Chapter 1: - Static Inner Class

---
+++ src/br/com/gatto/chapter1/bird/Toucan.java
@@ -0,0 +1,7 @@
+package br.com.gatto.chapter1.bird;
+
+public class Toucan {
+	public static class Beak {
+
+	}
+}
---
+++ src/br/com/gatto/chapter1/watcher/BirdWatcher.java
@@ -0,0 +1,7 @@
+package br.com.gatto.chapter1.watcher;
+
+import static br.com.gatto.chapter1.bird.Toucan.Beak;
+
+public class BirdWatcher {
+	Beak beak;
+}
