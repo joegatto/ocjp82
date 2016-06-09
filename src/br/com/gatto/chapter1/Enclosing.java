e20e3f7a8b7ab8333ff251c8c58755f175b98243
João Gatto <joaocarlosgatto@hotmail.com>
2016-06-09 11:17:32 -0300

    Chapter 1: - Static Inner Class - Exercises

--- src/br/com/gatto/chapter1/Enclosing.java
+++ src/br/com/gatto/chapter1/Enclosing.java
@@ -1,8 +1,13 @@
 package br.com.gatto.chapter1;
 
 public class Enclosing {
+	public int test = 1;
+
 	static class Nested {
 		private int price = 6;
+
+		static void test() {
+		}
 	}
 
 	public static void main(String[] args) {
---
+++ src/br/com/gatto/chapter1/exercises/Employee.java
@@ -0,0 +1,30 @@
+package br.com.gatto.chapter1.exercises;
+
+/**
+ * Exercise 1
+ * Explanation:
+ * 
+ */
+public class Employee {
+	public int employeeId;
+	public String firstName, lastName;
+	public int yearStarted;
+
+	@Override
+	public int hashCode() {
+		return employeeId;
+	}
+
+	public boolean equals(Employee e) {
+		return this.employeeId == e.employeeId;
+	}
+
+	public static void main(String[] args) {
+		Employee one = new Employee();
+		one.employeeId = 101;
+		Employee two = new Employee();
+		two.employeeId = 101;
+		if(one.equals(two)) System.out.println("Success");
+		else System.out.println("Failure");
+	}
+}
